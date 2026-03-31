49029c9a79b39438577f5c16401d04ccc72e542c
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-03-31 10:39:06 +0100

    feat(config): configure SSR settings and add Vite and Nitro options

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,12 +2,26 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/tailwind.css'],
 
   runtimeConfig: {
     public:{
       apiBase: 'http://127.0.0.1:8000/api'
+    }
+  },
+
+  vite: {
+    server: {
+      middlewareMode: false
+    }
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: false,
+      routes: ['/sitemap.xml', '/robots.txt']
     }
   }
 })
